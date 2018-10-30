308c4a4ba811d55cf1a4784fe646edaa73107e5d
Arthur <a.goldstein@outlook.fr>
2018-10-30 22:41:36 +0100

    tried to add asset management

--- src/Controller/HelloController.php
+++ src/Controller/HelloController.php
@@ -7,13 +7,6 @@ use Symfony\Component\Routing\Annotation\Route;
 class HelloController extends AbstractController
 
 {
-    /**
-    * @Route("/hello", name="hello")
-    */
-    public function hello()
-    {
-        return $this->render('hello.html.twig');
-    }
 
     /**
     * @Route("/homepage", name="homepage")
@@ -22,5 +15,14 @@ class HelloController extends AbstractController
     public function homepage()
     {
         return $this->render('homepage.html.twig');
+    }
+
+    /**
+     * @Route("/booklist", name="booklist")
+     */
+
+    public function booklist()
+    {
+        return $this->render('booklist.html.twig');
     }
 }
